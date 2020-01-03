1397d4726c25909feec332435c967d2960cee6c3
JiYoung <qnfmtm666@gmail.com>
2020-01-03 21:34:44 +0900

    re

--- config/settings.py
+++ config/settings.py
@@ -87,7 +87,6 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 if DEBUG:
-
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
