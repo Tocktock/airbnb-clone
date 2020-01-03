4b2cfa3b8b251cb2284d234a7b8f96fa6e3348c0
JiYoung <qnfmtm666@gmail.com>
2020-01-03 20:51:49 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -161,12 +161,3 @@ LOGIN_URL = "/users/login/"
 LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 
 # Sentry
-
-if not DEBUG:
-    sentry_sdk.init(
-        dsn=os.environ.get("SENTRY_URL"),
-        integrations=[DjangoIntegration()],
-        # If you wish to associate users to errors (assuming you are using
-        # django.contrib.auth) you may enable sending PII data.
-        send_default_pii=True,
-    )
